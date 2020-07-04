3ad68933cea458976b80946f6b00d7e2b31b0b33
AlexGaponenko <7911429@gmail.com>
2020-07-04 10:08:06 +0300

    Add project files.

---
+++ HelloApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace HelloApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write ("введите имя ");
+            String Name = Console.ReadLine();
+            Console.WriteLine($"Привет {Name}");
+            Console.ReadKey();
+        }
+    }
+}
